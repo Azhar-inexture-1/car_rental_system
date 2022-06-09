bb0e772d8bae49093a263a321750f75e650d28f4
azhar-inexture <azhar.inexture@gmail.com>
2022-06-09 13:10:47 +0530

    user is able to login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from .views import (
     user_registration_view
 )
 
 urlpatterns = [
-    path('register/', user_registration_view, name='user-register')
+    path('register/', user_registration_view, name='user-register'),
+    path('login/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
