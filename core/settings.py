3ef39a8bc9ac68966f81976667c20976213aaacc
azhar-inexture <azhar.inexture@gmail.com>
2022-06-09 11:24:26 +0530

    Database config

--- core/settings.py
+++ core/settings.py
@@ -85,11 +85,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'PORT': os.environ.get('DB_PORT'),
-        'HOST': os.environ.get('DB_HOST')
+        #'NAME': os.environ.get('DB_NAME'),
+        #'USER': os.environ.get('DB_USER'),
+        #'PASSWORD': os.environ.get('DB_PASSWORD'),
+        #'PORT': os.environ.get('DB_PORT'),
+        'HOST': os.environ.get('DATABASE_URL')
     }
 }
 
