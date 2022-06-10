937e2381b1795dd4572431415deb9c41b2b67d45
azhar-inexture <azhar.inexture@gmail.com>
2022-06-10 17:36:53 +0530

    changed debug true

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4$tgkrn42cq38%drkhdofm2s=zc!h*qrvgm=n0hy&1r(t#zh=='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
