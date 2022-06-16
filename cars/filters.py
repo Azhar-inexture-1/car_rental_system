f76dda277fb12433e6d6fb942d34d6a3a4e885c6
azhar-inexture <azhar.inexture@gmail.com>
2022-06-16 16:17:03 +0530

    car filter and password validation

---
+++ cars/filters.py
@@ -0,0 +1,14 @@
+from django_filters import rest_framework as filters
+from .models import Car
+
+
+class CarFilter(filters.FilterSet):
+    min_price = filters.NumberFilter(field_name="price", lookup_expr='gte')
+    max_price = filters.NumberFilter(field_name="price", lookup_expr='lte')
+
+    type = filters.CharFilter(field_name="type__name", lookup_expr='icontains')
+    brand = filters.CharFilter(field_name="brand__name", lookup_expr='icontains')
+
+    class Meta:
+        model = Car
+        fields = '__all__'
